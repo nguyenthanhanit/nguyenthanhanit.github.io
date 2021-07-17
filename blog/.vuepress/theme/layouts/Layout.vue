a4cefac079d02be36d7eb330c0e44dc20824ccb4
An Nguyen <nguyenan8196@gmail.com>
2021-07-17 11:36:51 +0700

    Responsive ui

--- blog/.vuepress/theme/layouts/Layout.vue
+++ blog/.vuepress/theme/layouts/Layout.vue
@@ -3,7 +3,7 @@
     <div class="container leading-relaxed">
         <Header/>
         <main class="grid md:grid-cols-4 gap-4 px-9 pb-9 bg-white rounded-b-3xl rounded-tl-3xl">
-          <Aside/>
+          <Aside :class="$page.path === '/' ? 'block' : 'hidden md:block'"/>
           <div class="md:col-span-3 md:pl-9">
             <template v-if="$page.frontmatter.home">
               <Home/>
